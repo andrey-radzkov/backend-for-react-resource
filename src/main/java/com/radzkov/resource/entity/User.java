fc57ef2b5f00d28f8bb8a2a8a1545a112cb157e3
liniyakamnya <liniyakamnya@gmail.com>
2018-05-23 23:45:16 +0300

    cascade save user options

--- src/main/java/com/radzkov/resource/entity/User.java
+++ src/main/java/com/radzkov/resource/entity/User.java
@@ -5,6 +5,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -39,7 +40,7 @@ public class User {
     @JsonIgnore
     private Basket basket;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "options_id")
     private UserOptions userOptions;
 
